80cd9969c62ffe2a54bec3ece359e65626d517b4
Raberj2 <jaredraberel@gmail.com>
2020-02-26 15:08:38 -0500

    Finished somethingUseful.sh

--- somethingUsefull.sh
+++ somethingUsefull.sh
@@ -1,3 +1,68 @@
 #!/bin/bash
 
 #something usefull for bash scripting assignment
+clear
+
+echo "Mission Planner for IL2: Sturmovick missions"
+
+
+echo "Select Aircraft: "
+echo "1: 109"
+echo "2: 111"
+echo "3: Ju88"
+read sel
+
+sleep 1
+
+clear
+
+echo "Total distance travel: "
+read dis
+
+clear
+
+case $sel in
+1)
+maxF=401
+(( ReqF = $dis / 3))
+#(( ReqF = $conv1 / 100))
+
+if [[ $dis -gt 1259 ]]
+then
+echo "To great a distance"
+else
+echo "$ReqF liters of fuel is required to make the travel of $dis km."
+fi
+
+;;
+2)
+maxF=8570
+#(( conv1 = $dis * 9))
+(( ReqF = $dis * 4))
+
+if [[ $dis -gt 2200 ]]
+then
+echo "To great a distance"
+else
+echo "$ReqF liters of fuel is required to make the travel of $dis km."
+fi
+
+;;
+
+3)
+maxF=3000
+(( conv1 = $dis * 9))
+(( ReqF = $conv1 / 10))
+
+if [[ $dis -gt 3000 ]]
+then
+echo "To great a distance"
+else
+echo "$ReqF liters of fuel is required to make the travel of $dis km."
+fi
+
+;;
+*)
+echo "Invalid selection"
+;;
+esac
