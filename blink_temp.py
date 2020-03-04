3f495940f2ce66cdd69f87962f3d29557d03916d
Raberj2 <jaredraberel@gmail.com>
2020-03-04 15:30:24 -0500

    Finished blink_temp.py

--- blink_temp.py
+++ blink_temp.py
@@ -8,14 +8,15 @@ tempSensor = Adafruit_DHT.DHT11
 #initialize the gpio
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
-GPIO.setup(13, GPIO.IN) #touch
-GPIO.setup(12, GPIO.IN) #hum
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP) #touch
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) #hum
 #This function will make the light blink once
 def readF(pin):
 	humidity, temperature = Adafruit_DHT.read_retry(tempSensor, 12)
 	temperature = temperature*9/5.0+32
 	if humidity is not None and temperature is not None:
 		tempFahr = '{0:0.1f}*F'.format(temperature)
+		print(tempFahr)
 	else:
 		print('Error Reading Sensor')
 	return tempFahr
